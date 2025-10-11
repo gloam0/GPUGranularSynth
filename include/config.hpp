025379696e58e1f92349484e10ceed00173a61aa
gloam0 <gloam.dev@gmail.com>
2025-10-11 14:17:16 -0400

    Add config vars

--- include/config.hpp
+++ include/config.hpp
@@ -10,6 +10,15 @@
 
 namespace config {
 
+// Audio
+    constexpr int SAMPLE_RATE = 48000;
+    constexpr int BLOCK_SIZE = 256;
+    // Number of GPU-write-CPU-read buffers in the ring (>1), e.g., 3 for triple-buffering,
+    // induces BUFFER_RING_SIZE * BLOCK_SIZE / SAMPLE_RATE constant latency
+    constexpr int BUFFER_RING_SIZE = 3;
+// Audio (non-configurable)
+    constexpr int NUM_CHANNELS = 2;
+// CUDA
     constexpr int CUDA_SELECTED_DEVICE = 0;
 
 }
