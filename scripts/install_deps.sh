0900399bbd425d393b66b39a0f91e4b0f2feca0a
gloam0 <gloam.dev@gmail.com>
2025-10-13 18:17:17 -0400

    Update

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo apt-get -y install libasound2-dev libasound2-plugins alsa-utils libjack-jackd2-dev
+git submodule update --init --recursive
 
 # test audio with `speaker-test -c 2 -r 48000 -D default -t sine -l 1`
