1d449d442c123445ca9d725f43cb867f483c320f
gloam0 <gloam.dev@gmail.com>
2025-10-12 20:06:30 -0400

    Update cuda_print_device_info()

--- include/utils/cuda_dev_info.hpp
+++ include/utils/cuda_dev_info.hpp
@@ -5,7 +5,7 @@
 
 int cuda_get_device_count();
 cudaDeviceProp cuda_get_device_properties(int dev);
-void cuda_print_device_info(const cudaDeviceProp* p);
+void cuda_print_device_info(int dev_id, const cudaDeviceProp* p);
 void cuda_print_device_info_all();
 
 #endif //GPUGRANULARSYNTH_CUDA_DEV_INFO_HPP
--- src/utils/cuda_dev_info.cpp
+++ src/utils/cuda_dev_info.cpp
@@ -1,6 +1,8 @@
 #include <utils/utils.hpp>
 #include <utils/cuda_dev_info.hpp>
 
+#include "config.hpp"
+
 int cuda_get_device_count() {
     int deviceCount = 0;
     CUDA_CHECK(cudaGetDeviceCount(&deviceCount));
@@ -13,25 +15,26 @@ cudaDeviceProp cuda_get_device_properties(const int dev) {
     return prop;
 }
 
-void cuda_print_device_info(const cudaDeviceProp* p) {
-    std::cout << "  name:                   " << p->name << "\n"
-              << "  compute maj.min:        " << p->major << "." << p->minor << "\n"
-              << "  # SMs:                  " << p->multiProcessorCount << "\n"
-              << "  max threads per SM:     " << p->maxThreadsPerMultiProcessor << "\n"
-              << "  max resident threads:   " << p->multiProcessorCount * p->maxThreadsPerMultiProcessor << "\n"
-              << "  max threads per block:  " << p->maxThreadsPerBlock << "\n"
-              << "  max grid size:          " << p->maxGridSize[0] << ", "
-                  << p->maxGridSize[1] << ", " << p->maxGridSize[2] << ")\n"
-              << "  total global mem:       " << p->totalGlobalMem / (1024 * 1024) << " MB\n"
-              << "  shared mem per block:   " << p->sharedMemPerBlock / 1024 << " KB\n"
-              << "  warp size:              " << p->warpSize << "\n";
+void cuda_print_device_info(const int id, const cudaDeviceProp* p) {
+    std::cout << "Selected GPU: " << p->name << " (id " << id << ")\n";
+    IF_DEBUG(
+    std::cout << "    compute maj.min:        " << p->major << "." << p->minor << "\n"
+              << "    # SMs:                  " << p->multiProcessorCount << "\n"
+              << "    max threads per SM:     " << p->maxThreadsPerMultiProcessor << "\n"
+              << "    max resident threads:   " << p->multiProcessorCount * p->maxThreadsPerMultiProcessor << "\n"
+              << "    max threads per block:  " << p->maxThreadsPerBlock << "\n"
+              << "    max grid size:          " << p->maxGridSize[0] << ", "
+              << p->maxGridSize[1] << ", " << p->maxGridSize[2] << ")\n"
+              << "    total global mem:       " << p->totalGlobalMem / (1024 * 1024) << " MB\n"
+              << "    shared mem per block:   " << p->sharedMemPerBlock / 1024 << " KB\n"
+              << "    warp size:              " << p->warpSize << "\n";
+    );
 }
 
 void cuda_print_device_info_all() {
     int deviceCount = cuda_get_device_count();
     for (int dev = 0; dev < deviceCount; ++dev) {
-        std::cout << "Device " << dev << "\n";
         cudaDeviceProp prop = cuda_get_device_properties(dev);
-        cuda_print_device_info(&prop);
+        cuda_print_device_info(dev, &prop);
     }
 }
