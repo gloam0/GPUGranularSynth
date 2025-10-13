f53af7f38c9eb1840961f4a43135d845f14e051e
gloam0 <gloam.dev@gmail.com>
2025-10-12 22:26:38 -0400

    Add SPSCRingPlayer

--- include/audio/audio_juce.hpp
+++ include/audio/audio_juce.hpp
@@ -3,6 +3,28 @@
 
 #include <juce_audio_devices/juce_audio_devices.h>
 
+#include <dataflow/spsc_ring_consumer.hpp>
+
 bool juce_dm_setup(juce::AudioDeviceManager& dm);
 
-#endif //GPUGRANULARSYNTH_AUDIO_JUCE_H
+class SPSCRingPlayer : public juce::AudioIODeviceCallback {
+public:
+    SPSCRingPlayer(SPSCRingState& rs, std::vector<AudioBlock>& ring)
+        : consumer(rs, ring) {}
+
+    void audioDeviceAboutToStart(juce::AudioIODevice*) override { consumer.start(); }
+    void audioDeviceStopped() override { consumer.stop(); }
+
+    void audioDeviceIOCallbackWithContext(const float* const*, int,
+        float* const* outputChannelData, int numOutputChannels,
+        int numSamples, const juce::AudioIODeviceCallbackContext&) override
+    {
+        juce::ScopedNoDenormals no_denormals;
+        consumer.process(outputChannelData, numOutputChannels, numSamples);
+    }
+
+private:
+    SPSCRingConsumer consumer;
+};
+
+#endif //GPUGRANULARSYNTH_AUDIO
